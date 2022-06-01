6b006ca0491710865e936c16258110d49e388540
Code Sandbox <hello@codesandbox.io>
2022-06-01 21:27:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2df641a748a848ee295cad2614fa057c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5710eb8e834de203daa66c75aee7ede6",
+    "url": "https://igortadic.github.io/csb-cnpkm0/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "https://igortadic.github.io/csb-cnpkm0/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "1ac7a3ca4bf45a9e0ecb",
+    "url": "https://igortadic.github.io/csb-cnpkm0/static/js/main.8f926b63.chunk.js"
+  },
+  {
+    "revision": "13ae93e3df3be4a98e76",
+    "url": "https://igortadic.github.io/csb-cnpkm0/static/js/runtime-main.299aa8ee.js"
+  }
+]);
